98f179d44d037ebe5f182a6cef64d68d74c3f4f7
SAR-dev <sayedarrafi.bd@gmail.com>
2024-09-14 23:06:37 +0900

    up

--- src/pb.ts
+++ src/pb.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
-import { TypedPocketBase } from "@types/pocketbase"
+import { TypedPocketBase } from "./types/pocketbase"
 
 const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase
 
