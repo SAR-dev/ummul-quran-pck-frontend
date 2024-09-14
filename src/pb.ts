9d6f4080b3de3c8954fa39e990444200566a652d
SAR-dev <sayedarrafi.bd@gmail.com>
2024-09-14 22:59:29 +0900

    up

---
+++ src/pb.ts
@@ -0,0 +1,6 @@
+import PocketBase from "pocketbase";
+import { TypedPocketBase } from "./types/pocketbase"
+
+const pb = new PocketBase('http://127.0.0.1:8090') as TypedPocketBase
+
+export default pb;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: "./env"
+  envDir: "./env",
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+  },
 })
